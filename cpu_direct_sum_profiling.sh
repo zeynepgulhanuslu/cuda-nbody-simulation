84d286a7d0451664fe60a65f8a5e2c7a32f7c6db
ZEYNEP GULHAN USLU <zeynep.uslu@turkcell.com.tr>
2025-05-25 20:06:03 +0300

    bugfix on profiling scripts

--- cpu_direct_sum_profiling.sh
+++ cpu_direct_sum_profiling.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Usage: ./run_profiling.sh output_directory
-
 if [ $# -ne 1 ]; then
     echo "Usage: $0 output_directory"
     exit 1
@@ -12,13 +10,19 @@ mkdir -p "$OUTDIR"
 
 N_LIST=(200 300 500 1000 2000 100000 500000)
 STEPS_LIST=(1000 2000 3000 5000)
+RESULT_FILE="${OUTDIR}/all_result.txt"
+: > "$RESULT_FILE"
 
 for N in "${N_LIST[@]}"; do
     for STEPS in "${STEPS_LIST[@]}"; do
         OUTFILE="${OUTDIR}/N${N}_steps${STEPS}.hdf5"
         PROFILE_OUT="${OUTDIR}/cpu_profile_N${N}_steps${STEPS}.txt"
         echo "Running: N=$N, steps=$STEPS"
-        ./nbody_cpu "$OUTFILE" "$N" "$STEPS"
+
+        run_output=$(./nbody_cpu "$OUTFILE" "$N" "$STEPS")
+        total_time=$(echo "$run_output" | grep "Process completed! Total time:" | awk '{print $(NF-1)}')
+
+        echo "N=$N, Step=$STEPS, total_time=$total_time" | tee -a "$RESULT_FILE"
         gprof ./nbody_cpu gmon.out > "$PROFILE_OUT"
         rm -f gmon.out
     done
--- gpu_direct_sum_profiling.sh
+++ gpu_direct_sum_profiling.sh
@@ -12,12 +12,18 @@ mkdir -p "$OUTDIR"
 
 N_LIST=(200 300 500 1000 2000 100000 500000)
 STEPS_LIST=(1000 2000 3000 5000)
+RESULT_FILE="${OUTDIR}/all_result.txt"
+: > "$RESULT_FILE"
 
 for N in "${N_LIST[@]}"; do
     for STEPS in "${STEPS_LIST[@]}"; do
         OUTFILE="${OUTDIR}/N${N}_steps${STEPS}_gpu.hdf5"
         PROFILE_OUT="${OUTDIR}/ncu_profile_N${N}_steps${STEPS}.ncu-rep"
         echo "Running (GPU): N=$N, steps=$STEPS"
-        ncu --set full --target-processes all --export "$PROFILE_OUT" ./nbody_gpu "$OUTFILE" "$N" "$STEPS"
+
+        run_output=$(ncu --set full --target-processes all --export "$PROFILE_OUT" ./nbody_gpu "$OUTFILE" "$N" "$STEPS" 2>&1)
+        total_time=$(echo "$run_output" | grep "Process completed! Total time:" | awk '{print $(NF-1)}')
+
+        echo "N=$N, Step=$STEPS, total_time=$total_time" | tee -a "$RESULT_FILE"
     done
 done
